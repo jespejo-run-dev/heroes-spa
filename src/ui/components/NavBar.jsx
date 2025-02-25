3630e3ae02d239ef90a39d5526467ac46130f0ae
Jorge Espejo Ibacache <jorge.espejo.ibacache@gmail.com>
2025-02-25 10:25:56 -0300

    Navigate push/replace - useNavigate

--- src/ui/components/NavBar.jsx
+++ src/ui/components/NavBar.jsx
@@ -1,7 +1,16 @@
-import { Link, NavLink } from 'react-router';
+import { Link, NavLink, useNavigate } from 'react-router';
 
 
 export const Navbar = () => {
+
+    const navigate = useNavigate();
+
+    const onLogout = () => {
+        navigate('/login', {
+            replace: true,
+        });
+    }
+
     return (
         <nav className="navbar navbar-expand-sm navbar-dark bg-dark p-2">
             
@@ -39,6 +48,7 @@ export const Navbar = () => {
 
                     <button
                         className='nav-item nav-link btn'
+                        onClick={onLogout}
                     >
                         Logout
                     </button>
