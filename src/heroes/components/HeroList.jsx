7dcc5c09bc8d0a0289f8b663825bfa4085ff9097
Jorge Espejo Ibacache <jorge.espejo.ibacache@gmail.com>
2025-02-25 10:58:12 -0300

    Tarjetas con informacion del heroe

--- src/heroes/components/HeroList.jsx
+++ src/heroes/components/HeroList.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+
+import { HeroCard } from './'
 import { getHeroeByPublisher } from '../helpers'
 
 export const HeroList = ({publisher}) => {
@@ -6,14 +7,15 @@ export const HeroList = ({publisher}) => {
     const heroes = getHeroeByPublisher(publisher)
 
   return (
-        <ul>
+        <div className='row rows-cols-1 row-cols-md-3 g-3'>
             {
                 heroes.map(hero => (
-                    <li key={hero.id}>
-                        {hero.superhero}
-                    </li>
+                    <HeroCard 
+                        key={hero.id}
+                        {...hero}    
+                    />
                 ))
             }
-        </ul>
+        </div>
     )
 }
