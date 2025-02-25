c098112b10a0132a82b5dba03715ce1b9e32687b
Jorge Espejo Ibacache <jorge.espejo.ibacache@gmail.com>
2025-02-25 14:24:45 -0300

    Leer argumentos por URL

--- src/heroes/routes/HeroesRoutes.jsx
+++ src/heroes/routes/HeroesRoutes.jsx
@@ -14,7 +14,7 @@ export const HeroesRoutes = () => {
               <Route path="dc" element={<DCPage />} />
 
               <Route path="search" element={<SearchPage />} />
-              <Route path="hero" element={<HeroPage />} />
+              <Route path="hero/:id" element={<HeroPage />} />
               {/* Search, Hero by id */}
 
               <Route path="/" element={<Navigate to='/marvel' />} />
