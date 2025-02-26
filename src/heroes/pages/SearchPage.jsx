d5079b6b629fa57b7ad3e30b21927bb23c654948
Jorge Espejo Ibacache <jorge.espejo.ibacache@gmail.com>
2025-02-26 12:55:20 -0300

    Mostrar listado de heroes

--- src/heroes/pages/SearchPage.jsx
+++ src/heroes/pages/SearchPage.jsx
@@ -3,6 +3,7 @@ import queryString from "query-string";
 
 import { useForm } from "../../hooks/useForm"
 import { HeroCard } from "../components"
+import { getHeroesByName } from "../helpers";
 
 
 export const SearchPage = () => {
@@ -11,9 +12,10 @@ export const SearchPage = () => {
   const location = useLocation(); 
 
   const {q = ''} = queryString.parse(location.search);  
+  const heroes = getHeroesByName(q);
 
   const {searchText, onInputChange} = useForm({
-    searchText: '',
+    searchText: q,
   });
 
   const onSearchSubmit = (e) => {
@@ -66,7 +68,11 @@ export const SearchPage = () => {
             No hero with <b> { q }</b>
           </div>
 
-          {/* <HeroCard {...hero} /> */}
+          {
+            heroes.map(hero => (
+              <HeroCard key={hero.id} {...hero} />
+            ))
+          }
 
         </div>
       </div>
