4b1685792c4660ad8d9fd2979516ddf970a8df4e
Jorge Espejo Ibacache <jorge.espejo.ibacache@gmail.com>
2025-02-26 10:55:22 -0300

    Nota: useMemo

--- src/heroes/components/HeroList.jsx
+++ src/heroes/components/HeroList.jsx
@@ -1,10 +1,11 @@
 
 import { HeroCard } from './'
 import { getHeroeByPublisher } from '../helpers'
+import { useMemo } from 'react'
 
 export const HeroList = ({publisher}) => {
 
-    const heroes = getHeroeByPublisher(publisher)
+    const heroes = useMemo(() => getHeroeByPublisher(publisher), [publisher])
 
   return (
         <div className='row rows-cols-1 row-cols-md-3 g-3'>
