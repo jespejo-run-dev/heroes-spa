b2ecf9fd9d0a7a4553776f37e568b3ad9c547b1f
Jorge Espejo Ibacache <jorge.espejo.ibacache@gmail.com>
2025-02-27 15:43:25 -0300

    Recordar ultima pagina visitada

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -10,9 +10,11 @@ export const LoginPage = () => {
 
   const onLogin = () => {
 
+    const lastPath = localStorage.getItem('lastPath') || '/';
+
     login('jespejo')
 
-    navigate("/", {
+    navigate(lastPath, {
       replace: true
     })
   }
--- src/router/PublicRoute.jsx
+++ src/router/PublicRoute.jsx
@@ -5,8 +5,10 @@ import { Navigate } from "react-router";
 export const PublicRoute = ({children}) => {
 
     const {logged} = useContext(AuthContext);
+    const lastPath = localStorage.getItem('lastPath') || '/';
+
 
   return (!logged)
   ? children
-  : <Navigate to="/marvel" />;
+  : <Navigate to={lastPath} />;
 }
