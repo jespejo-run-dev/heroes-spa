3c7542b793ee4c75fdbdaeb19b366995e3d5e7bb
Jorge Espejo Ibacache <jorge.espejo.ibacache@gmail.com>
2025-02-27 11:36:09 -0300

    Logout del usuario

--- src/auth/context/AuthProvider.jsx
+++ src/auth/context/AuthProvider.jsx
@@ -35,11 +35,21 @@ export const AuthProvider = ({children}) => {
 
     dispatch(action);
   }
+
+  const logout = () => {
+    localStorage.removeItem('user');
+    const action = {
+      type: types.logout,
+    }
+    dispatch(action);
+  }
     
   return (
     <AuthContext.Provider value={{
       ...authState,
+      // methods
       login,
+      logout
     }}>
         {children}
     </AuthContext.Provider>
