ba048efd71403ea3fdda50456f1f386fd2571a65
Jorge Espejo Ibacache <jorge.espejo.ibacache@gmail.com>
2025-02-27 10:46:33 -0300

    Login de un usuario

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,10 +1,17 @@
+import { useContext } from "react";
 import { useNavigate } from "react-router"
+import { AuthContext } from "../context/AuthContext";
 
 export const LoginPage = () => {
+
+  const {login} = useContext(AuthContext)
 
   const navigate = useNavigate();
 
   const onLogin = () => {
+
+    login('jespejo')
+
     navigate("/", {
       replace: true
     })
